2755e0f433c8cf39cb711b25b7af57220db280a1
vishvajeet-y <vishvaj_bt18@iiitkalyani.ac.in>
2021-08-29 21:36:47 +0530

    improve style

--- client/src/component/ShowCompany.js
+++ client/src/component/ShowCompany.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Card } from 'antd';
 import TagCard from './card/TagCard';
 import {Link} from 'react-router-dom'
+import '../css/showCompany.css'
 import { GlobalOutlined,TeamOutlined,SelectOutlined,LinkedinFilled} from '@ant-design/icons'
 const ShowCompany=(props) =>{
     const {company}=props
@@ -12,35 +13,42 @@ const ShowCompany=(props) =>{
     return (
        <Card >
  
-       <Card.Grid hoverable={false} style={{width:'50%',height:'80%',borderStyle:'none'}}>
+       <Card.Grid hoverable={false} className="card">
        
-    <div style={{display:'flex'}}>
-    <div style={{background:'#1177F4',width:'12%',marginRight:'2%', border:'1px solid black', borderRadius:'5%'}}>
-    <img src={image} alt=""/>
-    </div>
-   <div >
-      <div style={{display:'flex',fontSize:'15px',color:'blue'}}>
-      <Meta title={Name} />
-      <Meta avatar={<SelectOutlined />  } style={{marginLeft:'2%'}} />
-      </div>
-      <div style={{display:'flex',justifyContent:'space-between',marginTop:'4%',marginRight:'-30%'}}>
-
-      <div style={{color:'blue'}}>
-      <div style={{display:'flex',fontSize:'15px'}}>
-      <Link to="{website}"><Meta description={website}/> </Link>
-      <Meta avatar={<GlobalOutlined /> } style={{marginLeft:'2%'}}/>
-      </div>
-      <div >
-      <Meta description={'Location : '+Location } style={{marginTop:'2%'}}/>
-      <Meta avatar={<LinkedinFilled /> } style={{marginTop:'2%'}}/>
-      </div>
-      </div>
-      <div>
-      <Meta title={EmployeeNumber}  description={'Employees'} avatar={<TeamOutlined />} style={{fontSize:'15px'}}/>
-      </div>
-        <div>
-        <Meta title={'E-learning'}  description={'Industry'} avatar={<GlobalOutlined />} style={{fontSize:'15px'}}/>
+    <div className="companyinfo" >
+        <div className="imagebackground" style={{  background: "#1177F4",
+        
+        marginRight: "2%",
+        border: "1px solid black",
+        borderRadius: "5%"}}>
+        <img src={image} alt=""/>
         </div>
+   <div >
+              <div className="setTop setFont" style={{display:'flex',color:'blue'}}>
+              <Meta title={Name} />
+              <Meta avatar={<SelectOutlined />  } style={{marginLeft:'2%'}} />
+              </div>
+      <div className="companyinfo_info setTop" style={{justifyContent:'space-between',marginRight:'-30%'}}>
+
+          <div style={{color:'blue'}}>
+                  <div className="setFont setDisplay" >
+                  <Link to="{website}"><Meta description={website}/> </Link>
+                  <Meta avatar={<GlobalOutlined /> } style={{marginLeft:'2%'}}/>
+                  </div>
+
+                  <div >
+                  <Meta description={'Location : '+Location } className="setTop"/>
+                  <Meta avatar={<LinkedinFilled /> } className="setTop"/>
+                  </div>
+          </div>
+
+          <div>
+          <Meta title={EmployeeNumber}  description={'Employees'} avatar={<TeamOutlined />} className="setFont setTop"/>
+          </div>
+
+          <div>
+          <Meta title={'E-learning'}  description={'Industry'} avatar={<GlobalOutlined />} className="setFont setTop"/>
+          </div>
      
       </div>
       </div>
@@ -60,15 +68,15 @@ const ShowCompany=(props) =>{
      
        
   
-       <Card.Grid hoverable={false} style={{width:'100%',display:'flex',justifyContent:'space-between'}}>
-      <div style={{display:'flex'}}>
+       <Card.Grid hoverable={false} style={{width:'100%'}}>
+     
        <TagCard value={Description1} color={'#A1A7FC'}/>
        <TagCard value={Description2} color={'#A1A7FC'}/>
        <TagCard value={Description3} color={'#A1A7FC'}/>
        <TagCard value={Description4} color={'#A1A7FC'}/>
        <TagCard value={Location} color={'#A1A7FC'}/>
        <TagCard value={'world'} color={'#A1A7FC'}/>
-       </div>
+      
        </Card.Grid>
         </Card>
   
